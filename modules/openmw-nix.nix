a01f9a3392d2b99d30f381a473bc0726697fe2bc
waylon <waylondn@proton.me>
2025-09-03 22:43:21 -0400

    updates openmw-nix module

--- modules/openmw-nix.nix
+++ modules/openmw-nix.nix
@@ -1,23 +1,35 @@
 {
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
     openmw-nix = {
       url = "git+https://codeberg.org/PopeRigby/openmw-nix.git";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{
-    self,
-    nixpkgs,
-    openmw-nix,
-    ...
-  }: {
-    nixosConfiguration.nixos = nixpkgs.lib.nixosSystem {
-      home.packages = with openmw-nix.packages.x86_64-linux; [
-        delta-plugin
-        openmw-dev
-        openmw-validator
-        plox
+  outputs = inputs@{ self, nixpkgs, home-manager, openmw-nix, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.users.waylon = {
+            home.packages = with openmw-nix.packages.x86_64-linux; [
+              delta-plugin
+              openmw-dev
+              openmw-validator
+              momw-configurator
+              s3lightfixes
+              umo
+              curldl
+              groundcoverify
+              plox
+            ];
+          };
+        }
       ];
     };
   };
